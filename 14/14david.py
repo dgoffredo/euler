35d7e979d45b131a75f79577fbf9f29b402a1f28
David Goffredo <david@david-Aspire-5742G.(none)>
2013-10-20 19:31:15 -0400

    Fixed bug in problem 14

--- 14/14david.py
+++ 14/14david.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
-def collatz(n, count): # Increments count for convenience
+def collatz(n):
     if n % 2: # n odd
-        return  (3 * n + 1, count + 1)
+        return  3 * n + 1
     else:     # n even
-        return (n // 2, count + 1)
+        return n // 2
 
 # starting values from the initial cycle
-# 1 --> 2 --> 4 --> 2 --> 1
-startOfLongest = 2  # Where we start.
-lengthOfLongest = 3 # Length in hops.
+# 4 --> 2 --> 1
+startOfLongest = 4   # Where we start.
+longestSequence = {4, 2, 1} # Set of hops.
 
 for start in range(3, 1000000):
-    count = 0
     val = start
-    val, count = collatz(val, count)
+    values = {val}
     
-    while val != startOfLongest:
-        val, count = collatz(val, count)
-        break
-    if val == startOfLongest: # if we didn't break explicitly
+    while val not in longestSequence:
+        val = collatz(val)
+        values.add(val)
+
+    if val == startOfLongest: # we hit the top
         # Now we have a new max
         startOfLongest = start
-        lengthOfLongest += count
+        longestSequence |= values
+        print('New max of {} hops found starting from {}'.format(len(longestSequence), startOfLongest))
 
 print(startOfLongest) # the answer
-# print(lengthOfLongest)
+
